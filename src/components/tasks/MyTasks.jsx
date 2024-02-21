988fb0f3a8449dba30417c488a51ceebdfc7e1b2
habib-153 <h.r.sihab155@gmail.com>
2024-02-22 00:28:27 +0600

    task details modal created and done

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -2,21 +2,29 @@ import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateStatus, userTasks } from "../../redux/features/tasks/taskSlice";
+import TaskDetailsModal from "./TaskDetailsModal";
 
 const MyTasks = () => {
   const {tasks, userTask } = useSelector((state) => state.taskSlice);
   const { name: userName } = useSelector((state) => state.userSlice)
   const dispatch = useDispatch()
+  const [isOpen, setIsOpen] = useState(false)
+  const [taskId, setTaskId] = useState(0)
 
   useEffect(()=>{
     dispatch(userTasks(userName))
   },[dispatch, userName, tasks])
 
+  const handleModal = id =>{
+    setTaskId(id)
+    setIsOpen(!isOpen)
+  }
   return (
     <div>
+      <TaskDetailsModal isOpen={isOpen} setIsOpen={setIsOpen} id={taskId}/>
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
         {userTask.map(item => (<div
@@ -25,7 +33,7 @@ const MyTasks = () => {
         >
           <h1>{item.title}</h1>
           <div className="flex gap-3">
-            <button className="grid place-content-center" title="Details">
+            <button onClick={()=>handleModal(item.id)} className="grid place-content-center" title="Details">
               <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
             </button>
             <button onClick={()=> dispatch(updateStatus({id: item.id, status:"done"}))} className="grid place-content-center" title="Done">
